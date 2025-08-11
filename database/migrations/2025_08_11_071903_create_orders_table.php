0e64045d4f586366a9e911c88b12158bc880947e
KeyndraPrawira <keyndraardhiprawira@gmail.com>
2025-08-11 15:11:21 +0700

    api laravel-flutter

---
+++ database/migrations/2025_08_11_071903_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_produk');
+            $table->bigInteger('id_kategori');
+            $table->timestamps();
+
+            $table->foreign('id_produk')->references('id')->on('produks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
